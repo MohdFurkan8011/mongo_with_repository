1be5947b05887459963a77c8c4429a3c665a0693
MohdFurkan <mfs0786siwal@gmail.com>
2021-06-25 09:58:30 +0530

    Init

---
+++ src/main/java/com/mongo/repository/mongo_repository/MongoRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.mongo.repository.mongo_repository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoRepositoryApplication.class, args);
+	}
+
+}
